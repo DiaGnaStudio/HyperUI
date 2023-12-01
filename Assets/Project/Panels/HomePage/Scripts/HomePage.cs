19e64c7e4dbd072f1c058f1a820b68152dc2214d
Usef Farahmand <yoseffarahmand94@gmail.com>
2023-12-01 21:35:28 +0330

    feat add home page

---
+++ Assets/Project/Panels/HomePage/Scripts/HomePage.cs
@@ -0,0 +1,19 @@
+using DiaGna.UserInterface;
+
+namespace Uds.HyperUI.Home
+{
+    public class HomePage : UIPageBase
+    {
+        protected override void OnLoadPage()
+        {
+        }
+
+        protected override void OnOpenPage()
+        {
+        }
+
+        protected override void OnClosePage()
+        {
+        }
+    }
+}
